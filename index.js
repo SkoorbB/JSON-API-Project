fab3a0a1a10143ded1f9a24f58a53d3371d1240e
Brooks <Skoorb412785@yahoo.com>
2020-03-04 13:50:15 -0500

    have index running off 5000

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const parser = require('body-parser');
-const Review = require('./db/WineReview'); 
+const Review = require('./models/Wine'); 
 
 app.use(parser.json());
 
 app.get("/", (req, res) => {
     Review.find({}).then(reviews =>{
         res.json(reviews)
-    })
-})
+    });
+});
 
 app.listen(5000, () => {
     console.log('running 5000')
