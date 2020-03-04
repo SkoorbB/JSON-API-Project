f139f369e19e896b237df7ba3a0146782888dcee
Brooks <Skoorb412785@yahoo.com>
2020-03-04 12:23:23 -0500

    base index

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+const Review = require('../db/WineReview'); 
+
+app.use(parser.json());
+
+app.get("/", (req, res) => {
+    Review.find({}).then(reviews =>{
+        res.json(reviews)
+    })
+})
+
+
+app.listen(5000, () => {
+    console.log('running 5000')
+})
