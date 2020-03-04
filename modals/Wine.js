8408a7b47aa9c3670099434b9f4059e5f0d75b33
Brooks <Skoorb412785@yahoo.com>
2020-03-04 12:07:15 -0500

    made base model

--- modals/Wine.js
+++ modals/Wine.js
@@ -1,8 +1,16 @@
 const mongoose = require('../db/connection'); 
 const Schema = mongoose.Schema
 
-const Review = new Schema {
-
-}
+const Review = new Schema ({
+    points: String,
+    title: String,
+    designation: String,
+    variety: String,
+    region_1: String,
+    region_2: String,
+    province: String,
+    country: String,
+    winery: String
+})
 
 module.exports = mongoose.model('Review', Review)
